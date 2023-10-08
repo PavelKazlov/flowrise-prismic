13e1c46468fa629861548116e6688bf1a8b6501f
PavelKazlov <6956717@gmail.com>
2023-10-08 15:09:36 +0300

    Create Hero slice and helper components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
-// import Logo from "@/components/Logo";
-// import Bounded from "@/components/Bounded";
+import Logo from "@/components/Logo";
+import Bounded from "@/components/Bounded";
 
 export default async function Footer() {
   const client = createClient();
@@ -10,24 +10,26 @@ export default async function Footer() {
   const settings = await client.getSingle("settings");
 
   return (
-    // <Bounded as="footer">
-    <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
-      <Link href="/">{/* <Logo /> */}Logo</Link>
+    <Bounded as="footer">
+      <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+        <Link href="/">
+          <Logo />
+        </Link>
 
-      <p className="text-xs">
-        ©{new Date().getFullYear()} {settings.data.site_title}
-      </p>
+        <p className="text-xs">
+          ©{new Date().getFullYear()} {settings.data.site_title}
+        </p>
 
-      <ul className="flex">
-        {settings.data.navigation.map(({ link, label }) => (
-          <li key={label}>
-            <PrismicNextLink field={link} className="p-3">
-              {label}
-            </PrismicNextLink>
-          </li>
-        ))}
-      </ul>
-    </div>
-    // </Bounded>
+        <ul className="flex">
+          {settings.data.navigation.map(({ link, label }) => (
+            <li key={label}>
+              <PrismicNextLink field={link} className="p-3">
+                {label}
+              </PrismicNextLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Bounded>
   );
 }
